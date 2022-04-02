293bcd3dbc5035c8272d5cdafa1e7bb3aa8a88ec
morphisjustfun <mariojacobo2011@gmail.com>
2022-04-02 04:06:35 -0500

    Almost

--- Lab1-BplusTree/src/BplusTree.h
+++ Lab1-BplusTree/src/BplusTree.h
@@ -2,37 +2,43 @@
 #include <iostream>
 #include <queue>
 #include <vector>
-#define ORDER 21
+// #define ORDER 21
+//#define ORDER 20
+#define ORDER 3
 
 void sortArray(int *&array, int size);
 
 // BP node
 struct Node {
-  bool leaf;
-  // keys
-  int *keys;
-  // size
-  int size;
-  // children
-  Node **children;
+    bool leaf;
+    // keys
+    int keys[ORDER];
+    // size
+    int size;
+    // children
+    Node *children[ORDER + 1];
 
-  Node(int size = 1);
+    Node(int size = 1);
 
-  ~Node();
+    ~Node();
 };
 
 // BP tree
 class BplusTree {
-  Node *root;
-  Node *findParent(Node *cursor, Node *child);
-  void insertIntoInternalNode(Node *parentNode, Node *tempNode, int key);
+    Node *findParent(Node *cursor, Node *child);
+
+    void insertIntoInternalNode(Node *parentNode, Node *tempNode, int key);
 
 public:
-  BplusTree();
-  void insertar(int x);
-  std::vector<int> BFS();
+    Node *root;
 
-  ~BplusTree();
+    BplusTree();
+
+    void insertar(int x);
+
+    std::vector<int> BFS();
+
+    ~BplusTree();
 };
 
 
--- Lab1-BplusTree/src/main.cpp
+++ Lab1-BplusTree/src/main.cpp
@@ -4,42 +4,63 @@
 #include <iostream>
 
 void test() {
-  // Read file
-  std::ifstream texto;
-  texto.open("./output.txt");
+    // Read file
+    std::ifstream texto;
+    texto.open("../output.txt");
 
-  int datos[1000000];
-  int element;
-  if (texto.is_open()) {
-    int i = 0;
-    while (texto >> element) {
-      datos[i++] = element;
+    int datos[1000000];
+    int element;
+    if (texto.is_open()) {
+        int i = 0;
+        while (texto >> element) {
+            datos[i++] = element;
+        }
     }
-  }
 
-  int64_t tiempoInseccion = 0;
-  for (int t = 0; t < 10; ++t) {
-    auto tree = new BplusTree();
+    int64_t tiempoInseccion = 0;
+    for (int t = 0; t < 10; ++t) {
+        auto tree = new BplusTree();
 
-    std::chrono::steady_clock::time_point begin =
-        std::chrono::steady_clock::now();
-    for (int i = 0; i < 1000000; ++i) {
-      tree->insertar(datos[i]);
+        std::chrono::steady_clock::time_point begin =
+                std::chrono::steady_clock::now();
+        for (int i = 0; i < 1000000; ++i) {
+            tree->insertar(datos[i]);
+        }
+        std::chrono::steady_clock::time_point end =
+                std::chrono::steady_clock::now();
+
+        // Calcular tiempo
+        auto tiempo =
+                std::chrono::duration_cast<std::chrono::microseconds>(end - begin)
+                        .count();
+        tiempoInseccion += tiempo;
+
+        auto bfs = tree->BFS();
+        for (auto i: bfs) {
+            std::cout << i << std::endl;
+        }
+        delete tree;
     }
-    std::chrono::steady_clock::time_point end =
-        std::chrono::steady_clock::now();
 
-    // Calcular tiempo
-    auto tiempo =
-        std::chrono::duration_cast<std::chrono::microseconds>(end - begin)
-            .count();
-    tiempoInseccion += tiempo;
-  }
-
-  std::cout << "Tiempo de insercion: " << tiempoInseccion << std::endl;
+    std::cout << "Tiempo de insercion: " << tiempoInseccion << std::endl;
 }
 
 int main() {
-   test();
+//    test();
+    auto tree = new BplusTree();
+    tree->insertar(1);
+    tree->insertar(2);
+    tree->insertar(3);
+    tree->insertar(4);
+    tree->insertar(5);
+    tree->insertar(6);
+    tree->insertar(7);
+    tree->insertar(8);
+    tree->insertar(9);
+    tree->insertar(10);
+    auto bfs = tree->BFS();
+    for (auto i: bfs) {
+        std::cout << i << " ";
+    }
+    return 0;
 }
-
